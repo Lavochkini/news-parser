8863ff09b2bb4583cf6b1a36d0e470c24cc88d29
MrDaKSv <bodagarbatuk@gmail.com>
2025-08-26 17:15:35 +0300

    Test commit

---
+++ src/main/java/com/lake_team/fistserios/JavaFxApp.java
@@ -0,0 +1,47 @@
+package com.lake_team.fistserios;/*
+  @author Bogdan
+  @project fistserios
+  @class JavaFxApp
+  @version 1.0.0
+  @since 26.08.2025 - 15.37
+*/
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class JavaFxApp extends Application {
+    private static String[] savedArgs;
+    private ConfigurableApplicationContext springContext;
+
+    public static void launchApp(String[] args) {
+        savedArgs = args;
+        launch(args); // Запускає JavaFX Application
+    }
+
+    @Override
+    public void init() {
+        // Піднімаємо Spring Boot контекст
+        springContext = new SpringApplicationBuilder(NewsParserApplication.class)
+                .run(savedArgs);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Завантажуємо FXML через Spring
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main.fxml"));
+        fxmlLoader.setControllerFactory(springContext::getBean);
+
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setScene(scene);
+        stage.setTitle("News Parser");
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        springContext.close(); // Закриваємо Spring при виході
+    }
+}
---
+++ src/main/java/com/lake_team/fistserios/NewsParserApplication.java
@@ -0,0 +1,13 @@
+package com.lake_team.fistserios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsParserApplication {
+
+	public static void main(String[] args) {
+		// Виклик стартера JavaFX
+		JavaFxApp.launchApp(args);
+	}
+}
