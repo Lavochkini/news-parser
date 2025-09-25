62f3c7c8e9c92cef75599c6312e0162c14e60e4c
Stasss095 <sliuzers@gmail.com>
2025-09-25 15:02:05 +0200

    ff

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Main page loaded!");
+    // Тут можеш робити що завгодно, наприклад:
+    const header = document.querySelector("h1");
+    header.style.color = "blue";
+});
