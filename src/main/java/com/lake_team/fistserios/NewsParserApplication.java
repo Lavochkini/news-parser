efa0e4ea55fbb608bb4e9bf81fd617fde91e9fcf
MrDaKSv <bodagarbatuk@gmail.com>
2025-11-05 19:37:29 +0200

    Delete Java FX
    This time for migration is here

--- src/main/java/com/lake_team/fistserios/NewsParserApplication.java
+++ src/main/java/com/lake_team/fistserios/NewsParserApplication.java
@@ -1,16 +1,20 @@
 package com.lake_team.fistserios;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling      // keeps your scheduled jobs (already present)
-@EnableAsync           // enables @Async for background tasks
+@EnableScheduling      
+@EnableAsync
 public class NewsParserApplication {
+    private static String[] savedArgs;
+    private ConfigurableApplicationContext springContext;
+
 
     public static void main(String[] args) {
-        // Launch JavaFX starter
-        JavaFxApp.launchApp(args);
+        SpringApplication.run(NewsParserApplication.class, args);
     }
 }
